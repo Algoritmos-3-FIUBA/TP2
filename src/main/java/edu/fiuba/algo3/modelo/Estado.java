4dfb2a685c1e14cdd1425d57efd9871ecb67aaf1
LucasWaisten <43937195+LucasWaisten@users.noreply.github.com>
2020-07-19 11:12:56 -0300

    Creacion de Clase Estado

---
+++ src/main/java/edu/fiuba/algo3/modelo/Estado.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo;
+
+public interface Estado {
+
+    int sumarPuntos(int puntos);
+
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/EstadoIncorrecto.java
@@ -0,0 +1,9 @@
+package edu.fiuba.algo3.modelo;
+
+public class EstadoIncorrecto implements Estado{
+
+    @Override
+    public int sumarPuntos(int puntos){
+        return puntos;
+    }
+}
