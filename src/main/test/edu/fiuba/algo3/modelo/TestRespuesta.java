1d71dfe7a928a2e85e58a9f5b0e6ef1bd87afaaa
Guido-Panaccio <gpanaccio@fi.uba.ar>
2020-07-19 15:41:23 -0300

    Creo las Pruebas Respuesta

---
+++ src/main/test/edu/fiuba/algo3/modelo/TestRespuesta.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRespuesta {
+    @Test
+    //Creo Pruebas Respuesta
+}
