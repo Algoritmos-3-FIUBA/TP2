4b483ee1a88c54cdf7807803d5e7c0f1f20d6ccc
Guido-Panaccio <gpanaccio@fi.uba.ar>
2020-07-19 18:00:58 -0300

    Actualizo clase Respuesta

--- src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Respuesta.java
@@ -1,10 +1,15 @@
 package edu.fiuba.algo3.modelo;
 
 public class Respuesta {
-    int puntosRespuesta;
+    Jugador jugadorActual;
+    Opcion opcionElegida;
 
-    public void responderPregunta(Opcion opcion){
-        return(puntosRespuesta);
+    public void responderPregunta(Jugador jugador, Opcion opcion){
+        this.jugadorActual = jugador;
+        this.opcionElegida = opcion;
     }
+
+    public Jugador getJugador() { return jugadorActual;}
+    public Opcion getElegida() {return opcionElegida;}
 
 }
