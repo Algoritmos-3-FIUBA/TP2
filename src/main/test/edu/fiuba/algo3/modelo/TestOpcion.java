7f9c7d504aa878786ede786af23aabd797457208
JuanStenghele <jstenghele@fi.uba.ar>
2020-07-21 00:03:58 -0300

    Creo los tests unitarios de opcion

--- src/main/test/edu/fiuba/algo3/modelo/TestOpcion.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestOpcion.java
@@ -1,5 +1,19 @@
 package edu.fiuba.algo3.modelo;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestOpcion {
 
+    @Test
+    public void OpcionCorrectaDevuelve1Punto(){
+        OpcionCorrecta opcionCorrecta = new OpcionCorrecta();
+        assertEquals(1,opcionCorrecta.puntosASumar());
+    }
+
+    @Test
+    public void OpcionIncorrectaNoDevuelvePuntos(){
+        OpcionIncorrecta opcionIncorrecta = new OpcionIncorrecta();
+        assertEquals(0,opcionIncorrecta.puntosASumar());
+    }
 }
