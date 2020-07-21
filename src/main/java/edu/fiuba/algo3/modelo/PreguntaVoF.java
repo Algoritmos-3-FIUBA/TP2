3a4f3474c4727db9c67cf3a24f6faa3cbcb432ad
MatiasRotondo <mrotondo@fi.uba.ar>
2020-07-21 00:20:18 -0300

    Commit

---
+++ src/main/java/edu/fiuba/algo3/modelo/PreguntaVoF.java
@@ -0,0 +1,42 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+
+public class PreguntaVoF {
+    private String nombre;
+    private Opcion verdadero;
+    private Opcion falso;
+
+    private ArrayList opcionesDisponibles = new ArrayList();
+
+
+    public PreguntaVoF(String s) {
+        this.nombre = s;
+    }
+
+    public void verdaderoOpcionCorrecta() {
+        falso = new OpcionIncorrecta();
+        verdadero = new OpcionCorrecta();
+        this.aniadirOpciones();
+    }
+
+    private void aniadirOpciones() {
+        opcionesDisponibles.add(verdadero);
+        opcionesDisponibles.add(falso);
+    }
+
+    public void falsoOpcionCorrecta() {
+        falso = new OpcionCorrecta();
+        verdadero = new OpcionIncorrecta();
+        this.aniadirOpciones();
+    }
+
+    public void evaluarRespuestasFinales(ArrayList respuestasDeJugadores) {
+        Respuesta respuesta;
+        for(int i=0;i< respuestasDeJugadores.size();i++){
+            respuesta = (Respuesta) respuestasDeJugadores.get(i);
+            respuesta.otorgarPuntos(opcionesDisponibles);
+        }
+    }
+}
+
