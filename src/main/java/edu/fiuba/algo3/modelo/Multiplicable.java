caa6689c58f312fba069d81e3c43064e1577126c
Fernando <fer_giar99@hotmail.com>
2020-08-04 00:07:22 -0300

    Interfaz Multiplicable con sus clases que la implementan.

---
+++ src/main/java/edu/fiuba/algo3/modelo/Multiplicable.java
@@ -0,0 +1,7 @@
+package edu.fiuba.algo3.modelo;
+
+public class Multiplicable {
+    int multiplicarPuntos(int cantidadDePuntos, Multiplicador multiplicador){
+        ^(multiplicador.multiplicarPuntos(cantidadDePuntos));
+    }
+}
