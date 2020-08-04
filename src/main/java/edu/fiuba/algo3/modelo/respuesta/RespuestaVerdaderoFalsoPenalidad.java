26fcc8693fa889c4bfe27bf66f8b45801dc4a74b
MatiasRotondo <mrotondo@fi.uba.ar>
2020-08-04 12:28:54 -0300

    Refactor RespuestaVerdaderoFalsoPenalidad y creo clase RespuestaMultipleChoicePenalidad

--- src/main/java/edu/fiuba/algo3/modelo/respuesta/RespuestaVerdaderoFalsoPenalidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/respuesta/RespuestaVerdaderoFalsoPenalidad.java
@@ -1,17 +1,23 @@
 package edu.fiuba.algo3.modelo.respuesta;
 
+import edu.fiuba.algo3.modelo.Jugador;
 import edu.fiuba.algo3.modelo.multiplicador.Multiplicador;
 import edu.fiuba.algo3.modelo.opcion.Opcion;
-import edu.fiuba.algo3.modelo.pregunta.PreguntaMultipleChoice;
 
-public class RespuestaVerdaderoFalsoPenalidad extends Respuesta {
+public class RespuestaVerdaderoFalsoPenalidad implements Respuesta {
 
     private Opcion opcion;
     private Multiplicador multiplicador;
+    private Jugador jugador;
 
     public RespuestaVerdaderoFalsoPenalidad (Opcion opcion, Multiplicador multiplicador) {
         this.opcion = opcion;
         this.multiplicador = multiplicador;
+    }
+
+    public RespuestaVerdaderoFalsoPenalidad (Opcion opcion, Jugador jugador) {
+        this.opcion = opcion;
+        this.jugador = jugador;
     }
 
     public void otorgarPuntos() {
