db165563421bbaaec2df6878c13dbd272b57ca23
Guido-Panaccio <gpanaccio@fi.uba.ar>
2020-08-05 17:38:09 -0300

    Modifico la clase grupo y agrego testUnitarios

--- src/main/java/edu/fiuba/algo3/modelo/Grupo.java
+++ src/main/java/edu/fiuba/algo3/modelo/Grupo.java
@@ -1,30 +1,24 @@
 package edu.fiuba.algo3.modelo;
 
-import edu.fiuba.algo3.modelo.opcion.Opcion;
 import edu.fiuba.algo3.modelo.opcion.OpcionCorrecta;
-import edu.fiuba.algo3.modelo.opcion.OpcionIncorrecta;
-import edu.fiuba.algo3.modelo.respuesta.RespuestaMultipleChoice;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 
 public class Grupo {
     private String Nombre;
     HashSet<OpcionCorrecta> opcionesDelGrupo = new HashSet<OpcionCorrecta>();
-    private Puntos puntosAsignar = new Puntos(0);
 
     public Grupo(String nombre) {
-
         Nombre = nombre;
-
     }
 
     public void agregarOpcion(OpcionCorrecta opcionesDelGrupo) {
         this.opcionesDelGrupo.add(opcionesDelGrupo);
     }
 
-    public Puntos evaluarGrupo(HashSet<OpcionCorrecta> opcionesCorrctasDelGrupo, Puntos cantidadDePuntos) {
-        if (opcionesDelGrupo.equals(opcionesCorrctasDelGrupo)){
+    public Puntos evaluarGrupo(HashSet<OpcionCorrecta> opcionesElegidasDelGrupo, Puntos cantidadDePuntos) {
+        Puntos puntosAsignar = new Puntos(0);
+        if (opcionesElegidasDelGrupo.equals(opcionesDelGrupo)){
             puntosAsignar = cantidadDePuntos;
         }
         return(puntosAsignar);
