9b62ea1dc11fdd0badd652ce7d216b84850d7edb
Fernando <fer_giar99@hotmail.com>
2020-08-06 01:30:32 -0300

    Refactor PreguntaOrderedChoice

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/PreguntaOrderedChoice.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/PreguntaOrderedChoice.java
@@ -1,41 +1,40 @@
 package edu.fiuba.algo3.modelo.pregunta;
 
+import edu.fiuba.algo3.modelo.Grupo;
 import edu.fiuba.algo3.modelo.Puntos;
 import edu.fiuba.algo3.modelo.opcion.Opcion;
 import edu.fiuba.algo3.modelo.opcion.OpcionCorrecta;
 import edu.fiuba.algo3.modelo.opcion.OpcionIncorrecta;
+import edu.fiuba.algo3.modelo.respuesta.Respuesta;
+import edu.fiuba.algo3.modelo.respuesta.RespuestaGrupos;
+import edu.fiuba.algo3.modelo.respuesta.RespuestaMultiple;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 
-public class PreguntaOrderedChoice {
-/*    private String Nombre;
+public class PreguntaOrderedChoice extends Pregunta {
+    private String Nombre;
     private Puntos PuntosOtorgados;
-    LinkedList<OpcionCorrecta> opcionesCorrectas;
-    LinkedList<OpcionIncorrecta> opcionesIncorrectas;
+    LinkedList<Grupo> grupoCorrecto;
 
-    public PreguntaOrderedChoice(String nombre, int puntos, LinkedList<Opcion> opciones) {
+    public PreguntaOrderedChoice(String nombre, int puntos, LinkedList<Grupo> grupoCorrecto) {
 
         Nombre = nombre;
         PuntosOtorgados = new Puntos(puntos);
 
-        opcionesCorrectas = new LinkedList<OpcionCorrecta>();
-        opcionesIncorrectas = new LinkedList<OpcionIncorrecta>();
-
-        for (Opcion opcion : opciones)
-            opcion.agregarseALaListaCorrespondienteOrdenada(opcionesCorrectas, opcionesIncorrectas);
+        this.grupoCorrecto = grupoCorrecto;
     }
 
-    public void evaluarRespuestas(LinkedList<RespuestaOrderedChoice> respuestas) {
+    public void evaluarRespuestas(LinkedList<RespuestaGrupos> respuestas) {
 
-        for (RespuestaOrderedChoice respuesta : respuestas)
-            respuesta.otorgarPuntos(opcionesCorrectas, PuntosOtorgados);
+        for (RespuestaGrupos respuesta : respuestas) {
+            Puntos puntosAsignar;
+            LinkedList<Grupo> gruposElegidos;
+            gruposElegidos = respuesta.getGruposElegidos();
+            //Pregunta le delega a cada grupo correcto que se evalue, antes esa delegación la hacia Respuesta
+            puntosAsignar = grupoCorrecto.get(1).evaluarGrupo(gruposElegidos.get(1).getOpcionesDelGrupo(), PuntosOtorgados);
+            respuesta.otorgarPuntos(puntosAsignar);
+
+        }
     }
-
-    public LinkedList<OpcionCorrecta> getOpcionesCorrectas(){
-        return opcionesCorrectas;
-    }
-
-    public LinkedList<OpcionIncorrecta> getOpcionesIncorrectas(){
-        return opcionesIncorrectas;
-    }*/
 }
