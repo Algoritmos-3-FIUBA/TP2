01474c87cf88765d2defe86917817620c778dd1e
Guido-Panaccio <gpanaccio@fi.uba.ar>
2020-08-06 15:27:55 -0300

    Test RespuestaOrderedChoice

--- src/main/test/edu/fiuba/algo3/modelo/testEntregas/testPreguntaOrderedChoiceEntrega2.java
+++ src/main/test/edu/fiuba/algo3/modelo/testEntregas/testPreguntaOrderedChoiceEntrega2.java
@@ -1,131 +1,56 @@
 package edu.fiuba.algo3.modelo.testEntregas;
 
+import edu.fiuba.algo3.modelo.Jugador;
+import edu.fiuba.algo3.modelo.opcion.Opcion;
+import edu.fiuba.algo3.modelo.opcion.OpcionCorrecta;
+import edu.fiuba.algo3.modelo.opcion.OpcionIncorrecta;
+import edu.fiuba.algo3.modelo.pregunta.PreguntaMultipleChoice;
+import edu.fiuba.algo3.modelo.pregunta.PreguntaOrderedChoice;
+import edu.fiuba.algo3.modelo.respuesta.Respuesta;
+import edu.fiuba.algo3.modelo.respuesta.RespuestaMultiple;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class testPreguntaOrderedChoiceEntrega2 {
-/*    @Test
+    @Test
     public void CreoPreguntaOrderedChoiceConDosRespuestasCorrectasYJugadorRespondeCorrectamente() {
 
         Jugador Guido = new Jugador("Guido");
 
-        LinkedList<Opcion> opciones = new LinkedList<Opcion>();
+        LinkedList<OpcionCorrecta> opcionesCorrectas = new LinkedList<OpcionCorrecta>();
+        LinkedList<OpcionIncorrecta> opcionesIncorrectas = new LinkedList<OpcionIncorrecta>();
+
 
         OpcionIncorrecta primeraOpcion = new OpcionIncorrecta("1 + 1", 0);
         OpcionIncorrecta segundaOpcion = new OpcionIncorrecta("3 - 1", 0);
         OpcionCorrecta terceraOpcion = new OpcionCorrecta("2 + 2", 1);
         OpcionCorrecta cuartaOpcion = new OpcionCorrecta("2 * 2", 1);
 
-        opciones.add(primeraOpcion);
-        opciones.add(segundaOpcion);
-        opciones.add(terceraOpcion);
-        opciones.add(cuartaOpcion);
+        opcionesIncorrectas.add(primeraOpcion);
+        opcionesIncorrectas.add(segundaOpcion);
+        opcionesCorrectas.add(terceraOpcion);
+        opcionesCorrectas.add(cuartaOpcion);
 
-        PreguntaOrderedChoice pregunta = new PreguntaOrderedChoice("¿Cuales de las siguientes cuentas tiene resultado 4?", 1, opciones);
+        PreguntaOrderedChoice pregunta = new PreguntaOrderedChoice("¿Cuales de las siguientes cuentas tiene resultado 4?", 1, opcionesCorrectas ,opcionesIncorrectas);
 
-        LinkedList<RespuestaOrderedChoice> respuestas = new LinkedList<RespuestaOrderedChoice>();
+        LinkedList<RespuestaMultiple> respuestas = new LinkedList<RespuestaMultiple>();
 
-        LinkedList<Opcion> opcionesSeleccionadas = new LinkedList<Opcion>();
+        HashSet<Opcion> opcionesSeleccionadas = new HashSet<Opcion>();
         opcionesSeleccionadas.add(terceraOpcion);
         opcionesSeleccionadas.add(cuartaOpcion);
 
 
-        RespuestaOrderedChoice respuesta = new RespuestaOrderedChoice(opcionesSeleccionadas, Guido);
+        RespuestaMultiple respuesta = new RespuestaMultiple(opcionesSeleccionadas, Guido);
 
         respuestas.add(respuesta);
 
         pregunta.evaluarRespuestas(respuestas);
 
         assertEquals(Guido.getPuntos().getCantidad(), 1);
-        assertTrue(pregunta.getOpcionesIncorrectas().contains(primeraOpcion));
-        assertTrue(pregunta.getOpcionesIncorrectas().contains(segundaOpcion));
-        assertTrue(pregunta.getOpcionesCorrectas().contains(terceraOpcion));
-        assertTrue(pregunta.getOpcionesCorrectas().contains(cuartaOpcion));
     }
-
-    @Test
-    public void CreoPreguntaOrderedChoiceConDosRespuestasCorrectasYJugadorRespondeIncorrectamenteEnOtroOrden() {
-
-        Jugador Guido = new Jugador("Guido");
-
-        LinkedList<Opcion> opciones = new LinkedList<Opcion>();
-
-        OpcionIncorrecta primeraOpcion = new OpcionIncorrecta("1 + 1", 0);
-        OpcionIncorrecta segundaOpcion = new OpcionIncorrecta("3 - 1", 0);
-        OpcionCorrecta terceraOpcion = new OpcionCorrecta("2 + 2", 1);
-        OpcionCorrecta cuartaOpcion = new OpcionCorrecta("2 * 2", 1);
-
-        opciones.add(primeraOpcion);
-        opciones.add(segundaOpcion);
-        opciones.add(terceraOpcion);
-        opciones.add(cuartaOpcion);
-
-        PreguntaOrderedChoice pregunta = new PreguntaOrderedChoice("¿Cuales de las siguientes cuentas tiene resultado 4?", 1, opciones);
-
-        LinkedList<RespuestaOrderedChoice> respuestas = new LinkedList<RespuestaOrderedChoice>();
-
-        LinkedList<Opcion> opcionesSeleccionadas = new LinkedList<Opcion>();
-        opcionesSeleccionadas.add(cuartaOpcion);
-        opcionesSeleccionadas.add(terceraOpcion);
-
-
-        RespuestaOrderedChoice respuesta = new RespuestaOrderedChoice(opcionesSeleccionadas, Guido);
-
-        respuestas.add(respuesta);
-
-        pregunta.evaluarRespuestas(respuestas);
-
-        assertEquals(Guido.getPuntos().getCantidad(), 0);
-        assertTrue(pregunta.getOpcionesIncorrectas().contains(primeraOpcion));
-        assertTrue(pregunta.getOpcionesIncorrectas().contains(segundaOpcion));
-        assertTrue(pregunta.getOpcionesCorrectas().contains(terceraOpcion));
-        assertTrue(pregunta.getOpcionesCorrectas().contains(cuartaOpcion));
-    }
-
-    @Test
-    public void CreoPreguntaMultipleChoiceConDosJugadoreUnoSoloObtienePuntaje() {
-
-        Jugador Guido = new Jugador("Guido");
-        Jugador Juanchi = new Jugador("Juanchi");
-
-        LinkedList<Opcion> opciones = new LinkedList<Opcion>();
-
-        OpcionIncorrecta primeraOpcion = new OpcionIncorrecta("1 + 1", 0);
-        OpcionIncorrecta segundaOpcion = new OpcionIncorrecta("3 - 1", 0);
-        OpcionCorrecta terceraOpcion = new OpcionCorrecta("2 + 2", 1);
-        OpcionCorrecta cuartaOpcion = new OpcionCorrecta("2 * 2", 1);
-
-        opciones.add(primeraOpcion);
-        opciones.add(segundaOpcion);
-        opciones.add(terceraOpcion);
-        opciones.add(cuartaOpcion);
-
-        PreguntaOrderedChoice pregunta = new PreguntaOrderedChoice("¿Cuales de las siguientes cuentas tiene resultado 4?", 1, opciones);
-
-        LinkedList<RespuestaOrderedChoice> respuestas = new LinkedList<RespuestaOrderedChoice>();
-
-        LinkedList<Opcion> opcionesSeleccionadasGuido = new LinkedList<Opcion>();
-        opcionesSeleccionadasGuido.add(cuartaOpcion);
-        opcionesSeleccionadasGuido.add(terceraOpcion);
-
-        RespuestaOrderedChoice respuestaGuido = new RespuestaOrderedChoice(opcionesSeleccionadasGuido, Guido);
-
-        LinkedList<Opcion> opcionesSeleccionadasJuachi = new LinkedList<Opcion>();
-        opcionesSeleccionadasJuachi.add(terceraOpcion);
-        opcionesSeleccionadasJuachi.add(cuartaOpcion);
-
-        RespuestaOrderedChoice respuestaJuachi = new RespuestaOrderedChoice(opcionesSeleccionadasJuachi, Juanchi);
-
-        respuestas.add(respuestaGuido);
-        respuestas.add(respuestaJuachi);
-
-        pregunta.evaluarRespuestas(respuestas);
-
-        assertEquals(Guido.getPuntos().getCantidad(), 0);
-        assertEquals(Juanchi.getPuntos().getCantidad(), 1);
-        assertTrue(pregunta.getOpcionesIncorrectas().contains(primeraOpcion));
-        assertTrue(pregunta.getOpcionesIncorrectas().contains(segundaOpcion));
-        assertTrue(pregunta.getOpcionesCorrectas().contains(terceraOpcion));
-        assertTrue(pregunta.getOpcionesCorrectas().contains(cuartaOpcion));
-    }*/
 }
