9eab16b27530985bd2f6b36f80e7d03c1a011799
JuanStenghele <jstenghele@fi.uba.ar>
2020-08-13 18:19:51 -0300

    Refactor: Actualizo Test pregunta multiple choice con penalidad

--- src/main/java/edu/fiuba/algo3/modelo/pregunta/PreguntaMultipleChoicePenalidad.java
+++ src/main/java/edu/fiuba/algo3/modelo/pregunta/PreguntaMultipleChoicePenalidad.java
@@ -2,10 +2,10 @@ package edu.fiuba.algo3.modelo.pregunta;
 
 import edu.fiuba.algo3.modelo.ColeccionOpciones;
 import edu.fiuba.algo3.modelo.Puntos;
+import edu.fiuba.algo3.modelo.amplificador.Amplificador;
 import edu.fiuba.algo3.modelo.opcion.Opcion;
 import edu.fiuba.algo3.modelo.respuesta.Respuesta;
 import edu.fiuba.algo3.modelo.respuesta.RespuestaMultiple;
-import java.util.HashSet;
 import java.util.LinkedList;
 
 public class PreguntaMultipleChoicePenalidad extends Pregunta {
@@ -13,22 +13,37 @@ public class PreguntaMultipleChoicePenalidad extends Pregunta {
     private ColeccionOpciones opcionesCorrectas;
     private ColeccionOpciones opcionesIncorrectas;
 
-    public PreguntaMultipleChoicePenalidad (String nombre, ColeccionOpciones opciones) {
+    public PreguntaMultipleChoicePenalidad(String nombre, ColeccionOpciones opciones) {
 
         this.nombre = nombre;
 
         opcionesCorrectas = new ColeccionOpciones();
         opcionesIncorrectas = new ColeccionOpciones();
 
-        opciones.separarEnGruposCorrespondientes(opcionesCorrectas,opcionesIncorrectas);
+        opciones.separarEnGruposCorrespondientes(opcionesCorrectas, opcionesIncorrectas);
     }
 
-    public void evaluarRespuestas (LinkedList<Respuesta> listaRespuestas) {
-        for (Respuesta respuesta : listaRespuestas) {
+    public void evaluarRespuestas(LinkedList<Respuesta> respuestas) {
+/*        for (Respuesta respuesta : respuestas) {
             Puntos puntosParciales = new Puntos(0);
             for (Opcion opcion : ((RespuestaMultiple) respuesta).getOpciones().getOpciones())
                 puntosParciales.sumarPuntos(opcion.puntosObtenidos());
             respuesta.otorgarPuntos(puntosParciales);
+        }*/
+
+        corregirRespuestas(respuestas);
+
+        for (Respuesta respuesta : respuestas) {
+            Puntos puntosParciales = new Puntos(0);
+            for (Opcion opcion : ((RespuestaMultiple) respuesta).getOpciones().getOpciones())
+                puntosParciales.sumarPuntos(opcion.puntosObtenidos());
+            respuesta.otorgarPuntos(puntosParciales);        }
+    }
+
+    private void corregirRespuestas(LinkedList<Respuesta> respuestas){
+        for (Respuesta respuesta : respuestas) {
+            if (((RespuestaMultiple)respuesta).getOpciones().tieneElementos(opcionesCorrectas))
+                respuesta.setCorrecta();
         }
     }
 }
