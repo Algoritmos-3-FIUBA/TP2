80823241c1df658c7a63c5fdd9ae7e9c0bff3881
Guido-Panaccio <gpanaccio@fi.uba.ar>
2020-08-16 16:46:22 -0300

    Paquete sonido con archivo .mp3

--- src/main/java/edu/fiuba/algo3/vista/App.java
+++ src/main/java/edu/fiuba/algo3/vista/App.java
@@ -6,14 +6,12 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.nio.file.Paths;
 
-import java.nio.file.Paths;
-
 public class App extends Application {
 
     private Stage escenario;
     private static final String TITULO_JUEGO = "Kahoot!";
-    private static final String ICONO_JUEGO = "src/main/java/edu/fiuba/algo3/vista/imagenes/favicon.png"; //PROXIMAMENTE
-    private static final String SONIDO_JUEGO = "sonidos/sonido_inicio.mp3"; //PROXIMAMENTE
+    private static final String ICONO_JUEGO = "src/main/java/edu/fiuba/algo3/vista/imagenes/favicon.png";
+    private static final String SONIDO_JUEGO = "src/main/java/edu/fiuba/algo3/vista/sonidos/kahoot-lobby-music.mp33"; //PROXIMAMENTE
     private static final int ANCHO = 800;
     private static final int ALTO = 500;
 
@@ -21,11 +19,14 @@ public class App extends Application {
     public void start(Stage escenarioInicial) throws Exception {
         this.escenario=escenarioInicial;
 
-        //escenarioInicial.getIcons().add(new Image(getClass().getResourceAsStream(ICONO_JUEGO)));
-        //escenarioInicial.getIcons().add(icono);
-
         escenarioInicial.getIcons().add(new Image(Paths.get(ICONO_JUEGO).toUri().toString()));
 
+/* Falta media
+        Media media = new Media(Paths.get(SONIDO_JUEGO).toUri().toString());
+        MediaPlayer musicaDeFondo = new MediaPlayer(media);
+        musicaDeFondo.setVolume(0.3);
+        musicaDeFondo.play();
+*/
         escenarioInicial.setTitle(TITULO_JUEGO);
 
         FormularioInicial formularioInicial = new FormularioInicial() ;
