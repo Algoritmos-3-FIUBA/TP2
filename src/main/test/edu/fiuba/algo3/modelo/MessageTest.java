066919b63a501ccb2fc9d7d9d70b11ae0365d70a
LucasWaisten <43937195+LucasWaisten@users.noreply.github.com>
2020-08-18 12:06:41 -0300

    Json Modificaciones Lucas y Fernando

--- src/main/test/edu/fiuba/algo3/modelo/MessageTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/MessageTest.java
@@ -1,11 +1,14 @@
 package edu.fiuba.algo3.modelo;
 
+import edu.fiuba.algo3.vista.LecturaDeArchivo;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MessageTest {
     @Test
     public void messageGreeting() {
+        LecturaDeArchivo lectura = new LecturaDeArchivo();
+
         Message message = new Message("Hello world!");
 
         assertEquals("Hello world!", message.greet());
