4d48fbda07decc9503d62612abd595d662e4047b
Guido-Panaccio <gpanaccio@fi.uba.ar>
2020-08-19 22:05:54 -0300

    Panel Jugador General

--- src/main/java/edu/fiuba/algo3/modelo/SistemaEscenas.java
+++ src/main/java/edu/fiuba/algo3/modelo/SistemaEscenas.java
@@ -1,19 +1,26 @@
 package edu.fiuba.algo3.modelo;
 
 import edu.fiuba.algo3.modelo.pregunta.Pregunta;
+import edu.fiuba.algo3.vista.FormularioInicial;
+import edu.fiuba.algo3.vista.PanelJugadorGeneral;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import static edu.fiuba.algo3.modelo.Kahoot.actualizarEscena;
 
 public class SistemaEscenas {
+    private LinkedList<Jugador> jugadores;
 
     private Queue<Turno> turnos = new LinkedList();
 
     public SistemaEscenas(LinkedList<Jugador> jugadores, LinkedList<Pregunta> preguntas, HashMap<Pregunta, String> plantillasPreguntas) throws IOException {
+        this.jugadores = jugadores;
         for(Pregunta pregunta : preguntas){
             turnos.add(new TurnoJugador(jugadores,pregunta,plantillasPreguntas.get(pregunta)));
             //turnos.add(new TurnoPuntuaciones(jugadores,"url"));
@@ -21,6 +28,16 @@ public class SistemaEscenas {
     }
 
     public void siguienteEscena() throws IOException {
+
+  /*      PanelJugadorGeneral panelJugadorGeneral = new PanelJugadorGeneral() ;
+        Scene scene = new Scene(panelJugadorGeneral, 500, 500);
+        Stage escenarioPanel = new Stage();
+        panelJugadorGeneral.puntosJugadores(escenarioPanel, jugadores);
+        escenarioPanel.setScene(scene);
+
+*/
+
+
         Turno turnoSiguiente = turnos.remove();
         turnoSiguiente.mostrarEscena();
     }
